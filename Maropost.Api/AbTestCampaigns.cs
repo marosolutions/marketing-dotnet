64a2e31ffd60de88a5b3c6629e3b7491d654fc34
Santosh <sas@codebee.dk>
2019-04-04 23:51:29 +0545

    master text format

--- Maropost.Api/AbTestCampaigns.cs
+++ Maropost.Api/AbTestCampaigns.cs
@@ -7,7 +7,7 @@ namespace Maropost.Api
     public class AbTestCampaigns : _BaseApi
     {
         public AbTestCampaigns(int accountId, string authToken)
-            :base(accountId, authToken, "campaigns")
+            : base(accountId, authToken, "campaigns")
         { }
     }
 }
